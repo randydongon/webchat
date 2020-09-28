706a068abf517ab893ca0d0e62aa4498fc85a47d
Randy A. Dongon <bodrorandy@gmail.com>
2020-09-28 21:14:26 +0800

    Updates

---
+++ precache-manifest.ea176d4cca95c4f7e45256c11e77f7bc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c014cf488f22adb82047068d140ea1c",
+    "url": "/webchat/index.html"
+  },
+  {
+    "revision": "95a8e4f919b6ac3c4aae",
+    "url": "/webchat/static/css/2.fddaa3a5.chunk.css"
+  },
+  {
+    "revision": "df45310dc9b0c5fc58b9",
+    "url": "/webchat/static/css/main.427aa756.chunk.css"
+  },
+  {
+    "revision": "95a8e4f919b6ac3c4aae",
+    "url": "/webchat/static/js/2.6cc8d643.chunk.js"
+  },
+  {
+    "revision": "c0d3c633a43d15bf74d5ca3a17381f64",
+    "url": "/webchat/static/js/2.6cc8d643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df45310dc9b0c5fc58b9",
+    "url": "/webchat/static/js/main.e0312459.chunk.js"
+  },
+  {
+    "revision": "2db9219aadacacc9c59a",
+    "url": "/webchat/static/js/runtime-main.627c0367.js"
+  },
+  {
+    "revision": "9b5a702d1a500874eb2fb6e90ae83744",
+    "url": "/webchat/static/media/avaicon.9b5a702d.jpg"
+  },
+  {
+    "revision": "6c95cde86a278fcba194addbfc329b53",
+    "url": "/webchat/static/media/gloriya.6c95cde8.jpg"
+  },
+  {
+    "revision": "e8997b82c174981a1c6a89ad75da39e9",
+    "url": "/webchat/static/media/quezon.e8997b82.jpg"
+  }
+]);
